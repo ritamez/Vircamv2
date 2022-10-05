44761f3cde6e0c2df4c43b2387257421a1cef525
Rodolfo Tamez <rodolfo.tamez@gmail.com>
2022-10-05 22:37:30 +0200

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,26 @@ Support : codings.dev
 1. Preloader
 ----------------------------------------------*/
 
+
+
+const loader = document.querySelector('.app-preloader');
+const main = document.querySelector('.main');
+
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = 'none';
+
+    main.style.display = 'block';
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
+}
+
+init();
+
+
+
+
 // #region Preloader
 
 jQuery(function ($) {
